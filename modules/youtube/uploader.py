cf824ba55a4558ac238476e96677b7dd38c5175f
Joseph <mateuszpieczulis123@gmail.com>
2021-08-11 20:23:29 +0200

    retriable upload

--- modules/youtube/uploader.py
+++ modules/youtube/uploader.py
@@ -1,8 +1,13 @@
-import os
 import datetime
+import http.client
+import os
+import random
 from datetime import time
 
+import httplib2
+from apiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
+
 
 class YoutubeUploader():
     def __init__(self, youtube, fileManager):
@@ -11,6 +16,10 @@ class YoutubeUploader():
 
         self.categoryId = 10 # Music
         self.videoDir = '/media/joseph/HDD1/Py_projects/youtubeAutomatization/modules/storage/workInProgress'
+
+        self.maxRetries = 10
+        self.retriableStatusCodes = [500, 502, 503, 504]
+        self.retriableExceptions = (httplib2.HttpLib2Error, IOError, http.client.NotConnected, http.client.IncompleteRead, http.client.ImproperConnectionState, http.client.CannotSendRequest, http.client.CannotSendHeader, http.client.ResponseNotReady, http.client.BadStatusLine)
 
     def createTitleDefault(self, videoTitle):
         songName, authorName = str(videoTitle).split(' - ')
@@ -44,6 +53,9 @@ class YoutubeUploader():
         else:
             print('Upload failed, file not found')
 
+    def formatResponse(self, response):
+        return response
+
     def uploadRequest(self, title, description, videoPath):
         request_body = {
             'snippet': {
@@ -58,14 +70,46 @@ class YoutubeUploader():
             },
             'notifySubscribers': False
         }
-        mediaFile = MediaFileUpload(videoPath)
 
-
-        print('########## upload started ##########')
-        response_upload = self.youtube.videos().insert(
+        insertRequest = self.youtube.videos().insert(
             part='snippet,status',
             body=request_body,
-            media_body=mediaFile
-        ).execute()
+            media_body=MediaFileUpload(videoPath, chunksize=-1, resumable=True)
+        )
+        response = self.resumable_upload(insertRequest)
 
-        return response_upload
+        return self.formatResponse(response)
+
+    def resumable_upload(self, insertRequest):
+        response = None
+        error = None
+        retry = 0
+        while response is None:
+            try:
+                print ("Uploading file...")
+                status, response = insertRequest.next_chunk()
+                if response is not None:
+                    if 'id' in response:
+                        print ("Video id '%s' was successfully uploaded." % response['id'])
+                    else:
+                        exit("The upload failed with an unexpected response: %s" % response)
+            except HttpError as e:
+                if e.resp.status in self.retriableStatusCodes:
+                    error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+                                                                        e.content)
+                else:
+                    raise
+
+            except self.retriableExceptions as e:
+                error = "A retriable error occurred: %s" % e
+
+                if error is not None:
+                    print (error)
+                    retry += 1
+                if retry > self.maxRetries:
+                    exit("No longer attempting to retry.")
+
+                max_sleep = 2 ** retry
+                sleep_seconds = random.random() * max_sleep
+                print ("Sleeping %f seconds and then retrying..." % sleep_seconds)
+                time.sleep(sleep_seconds)
